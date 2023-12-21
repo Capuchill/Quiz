c7a3d465077509381478a740803de0d915a8ef02
Capuchill <orutvictoria05780@gmail.com>
2023-12-20 21:46:11 -0500

    si

---
+++ ciclos/src/CortaLarga.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CortaLarga {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        String larga = "";
+        String corta = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        
+        ArrayList<String> palabras = new ArrayList<>();
+        
+        String palabra;
+        System.out.print("¡Hola!¿Cuantas palabras deseas ingresar?->");
+        int numPalabras = leer.nextInt();
+        //System.out.print("Ingresa las palabras!");
+        
+        
+        
+
+        for (int i = 0; i < numPalabras; i++) {
+            System.out.print("->");
+            palabra = leer.nextLine();
+            System.out.print(palabra);
+            palabras.add(palabra);
+        }
+
+        System.out.println(palabras);
+
+        for (String element : palabras) {
+            
+            if (element.length() > larga.length()) {
+                larga = element;
+            } else{
+                if (element.length() < corta.length()) {
+                    corta = element; 
+                }
+            }
+        }
+        
+        System.out.println("La palabra mas larga es " + larga);
+        System.out.println("La palabra mas corta es " + corta);
+        
+
+    }
+}
